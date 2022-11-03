734297939eddb68931a7db88513e7188d87f5a21
KowsalyaMK <kowsalyamk92@gmail.com>
2022-11-04 00:25:02 +0800

    update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -179,6 +179,7 @@ const About = () => {
               className="img"
               src="../../images/me.png"
               width={500}
+              quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
               alt="Headshot"
             />
