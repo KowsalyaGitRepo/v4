d9198b85bd2eff9ae2e76d80f89e3f1d9c0e1b90
KowsalyaMK <kowsalyamk92@gmail.com>
2022-11-04 00:13:25 +0800

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-39f9a1d2154eb9590085.js"
   },
   {
-    "url": "app-438e35f47114d6be8d0d.js"
+    "url": "app-1aa535a6963509461ef5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "379fe746ecce192b0d9d4d18f4845e59"
+    "revision": "936972b93fabfd73499b830f6059d7e2"
   },
   {
     "url": "polyfill-51fd3860cf8afd0c4ff8.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/KowsalyaGitRepo/app-438e35f47114d6be8d0d.js`))) {
+  if (!resources || !(await caches.match(`/KowsalyaGitRepo/app-1aa535a6963509461ef5.js`))) {
     return await fetch(event.request)
   }
 
